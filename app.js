ab6db956e59624929ebfe35c53022579c553061f
vilepham <vilepham136@gmail.com>
2020-05-22 17:59:41 +0900

    finished button functions

--- app.js
+++ app.js
@@ -1,36 +1,64 @@
-const app = () => {
-    const content = document.querySelector('.content');
-    const rainBtn = document.querySelector('.rain');
-    const beachBtn = document.querySelector('.beach');
-    const rainVid = document.querySelector('#rain-vid');
-    const beachVid = document.querySelector('#beach-vid');
-    const song = document.querySelector('.song');
-    const sounds = document.querySelectorAll(".sound-picker button");
-    const outline = document.querySelector(".moving-outline circle");
-    const outlineLength = outline.getTotalLength();
-    const timeBtn = document.querySelectorAll(".time-btn button");
-    const timer = document.querySelector('.timer');
 
-    // click theme buttons to change bg to video
-    rainBtn.addEventListener('click', () => {
-        content.style.background = 'rgba(250, 140, 127, 0.363)';
-        rainVid.style.display = 'block';
-        beachVid.style.display = 'none';
+const content = document.querySelector('.content');
+const sounds = document.querySelectorAll('.theme button');
+
+const playBtn = document.querySelector('.play');
+const audio = document.querySelector('audio');
+const video = document.querySelector('video');
+const outline = document.querySelector('.moving-outline circle');
+const outlineLength = outline.getTotalLength();
+
+const timeBtn = document.querySelectorAll('.time-btn button');
+const timer = document.querySelector('.timer');
+let fakeDuration = 600;
+
+// Change bachground and audio theme
+sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      audio.src = this.dataset.sound;
+      video.src = this.dataset.video;
+      checkPlaying(audio);
     });
-    beachBtn.addEventListener('click', () => {
-        content.style.background = 'rgba(250, 140, 127, 0.363)';
-        beachVid.style.display = 'block';
-        rainVid.style.display = 'none';
-    });
+  });
+
+// Play pause button
+playBtn.addEventListener('click', () => {
+    checkPlaying(audio);
+});
+
+// Play pause audio function
+const checkPlaying = (audio) => {
+    if (audio.paused) {
+        audio.play();
+        playBtn.src = './svg/pause.svg';
+        video.play();
+    } else {
+        audio.pause();
+        playBtn.src = './svg/play.svg';
+        video.pause();
+    }
+}
+
+// Set timer
+
+// Get outline length
 
 
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
+timer.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
 
 
+// Animate the outline
+audio.ontimeupdate = function() {
+    let currentTime = audio.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elasped / 60);
+    timer.textContent = `${minutes}:${seconds}`;
+    console.log(elapsed);
 
-
-
-
-    //click play to start interval + start audio 
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
 };
 
-app();
